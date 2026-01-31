b00389693c636142ae4d4be20bfb9661c4a4c918
Thanasorn Dusadeeroj <64414782+vavaza09@users.noreply.github.com>
2026-01-31 20:06:14 +0700

    Lab4(Profile): Create ProfileComponent

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import ProfileCard from './components/ProfileCard.vue';
+
+</script>
+
+<template>
+    <div class=" flex flex-col md:flex-row items-center justify-center gap-8 min-h-screen
+     bg-gradient-to-br from-green-500 to-sky-400">
+    <ProfileCard 
+        background="./src/assets/workbg.jpg"
+        profileimg="./src/assets/profileimg.jpg"
+        name="Thanasorn Dusadeeroj"
+        subheading="Full-stack Developer"
+        followers="67k"
+        following="67k"
+        projects="67"
+        githubicon="./src/assets/githubicion.png"
+        facebookicon="./src/assets/facebookicon.png"
+        instagramicon="./src/assets/instagram.png"
+    />
+    <ProfileCard 
+        background="./src/assets/workbg.jpg"
+        profileimg="./src/assets/nodprofile.jpg"
+        name="Kanokpon suebyam"
+        subheading="UX/UI Designer"
+        followers="100M"
+        following="1"
+        projects="0"
+        githubicon="./src/assets/githubicion.png"
+        facebookicon="./src/assets/facebookicon.png"
+        instagramicon="./src/assets/instagram.png"
+    />
+    <ProfileCard 
+        background="./src/assets/workbg.jpg"
+        profileimg="./src/assets/gapprofile.jpg"
+        name="Supakorn ieamgomol"
+        subheading="Full-stack Developer"
+        followers="0"
+        following="0"
+        projects="1M"
+        githubicon="./src/assets/githubicion.png"
+        facebookicon="./src/assets/facebookicon.png"
+        instagramicon="./src/assets/instagram.png"
+    />
+    </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,80 @@
+<script setup>
+    defineProps([
+        "profileimg",
+        "background",
+        "name",
+        "subheading",
+        "followers",
+        "following",
+        "projects",
+        "githubicon" ,
+        "facebookicon",
+        "instagramicon"
+    ]);
+</script>
+
+<template>
+    <div class=" bg-white shadow-lg rounded-2xl
+    w-full max-w-[320px] overflow-hidden text-center">
+        <div class=" relative min-h-32 bg-cover
+         bg-center" :style="'background-image: url('+ background +')'">
+            <img :src="profileimg" alt="ProfileImage" class=" inline-block
+             w-32 h-32 rounded-full object-center object-cover
+              border-6 border-white absolute top-full 
+               left-1/2 transform -translate-1/2">
+        </div>
+        <!-- SubHeading -->
+        <div class=" px-6 pt-20 pb-6">
+            <h2 class=" font-bold text-gray-800
+             text-2xl mb-2">{{name}}</h2>
+
+            <h3 class=" text-gray-600 text-lg">
+                {{subheading}}
+            </h3>
+        </div>
+        <!-- Stat -->
+        <div class="p-6 border-t border-gray-200 flex
+         items-center justify-center gap-6">
+            <!-- Followers -->
+             <div>
+                <h4 class=" text-gray-600 text-sm">
+                    Followers
+                </h4>
+                <p class=" font-bold text-gray-800 text-2xl">
+                    {{followers}}
+                </p>
+             </div>
+                         <!-- Following -->
+             <div>
+                <h4 class=" text-gray-600 text-sm">
+                    Following
+                </h4>
+                <p class=" font-bold text-gray-800 text-2xl">
+                    {{following}}
+                </p>
+             </div>
+                         <!-- Project -->
+             <div>
+                <h4 class=" text-gray-600 text-sm">
+                    Projects
+                </h4>
+                <p class=" font-bold text-gray-800 text-2xl">
+                    {{projects}}
+                </p>
+             </div>
+        </div>
+        <div class=" px-6 pb-6 flex items-center">
+            <img :src="githubicon" alt="GitHub Link" class=" w-10 h-10
+             mx-auto cursor-pointer hover:scale-110
+              transition-transform duration-300">
+            <img :src="facebookicon" alt="facebook" class=" w-10 h-10
+             mx-auto cursor-pointer hover:scale-110
+              transition-transform duration-300">
+            <img :src="instagramicon" alt="instagram" class=" w-10 h-10
+             mx-auto cursor-pointer hover:scale-110
+              transition-transform duration-300">
+        </div>
+     </div>
+</template>
+
+<style scoped></style>
